a581435486173322ea3567d1e102195c7b7deed6
xSistted <pontakorn1223@gmail.com>
2026-02-06 02:47:05 +0700

    add card ui purpose

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
     return (
-        <nav className="sticky top-0 bg-white shadow-lg">
+        <nav className="sticky top-0 bg-white shadow-lg overflow-hidden z-50">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
                     <div className="flex-shrink-0">
--- apps/web/src/pages/home/page.tsx
+++ apps/web/src/pages/home/page.tsx
@@ -3,9 +3,9 @@ import Navbar from "../../components/NavBar";
 function Home() {
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div className="min-h-screen bg-gradient-to-br from blue-100 via-cyan-100 to-cyan-200">
-                <div className="container mx-auto px-4 py-16">
+                <div className="container mx-auto px-4 py-12">
                     <div className="text-center space-y-8">
                         {/* Main Title */}
                         <div className="relative">
@@ -18,25 +18,49 @@ function Home() {
                         <p className="text-4xl text-blue-400 font-bold">
                             Manage tasks with your teamate!
                         </p>
-                    {/* 3 Card Section */}
-                    <div className="flex justify-evenly">
-                        <div>
-                            <h3>Option 1</h3>
+                        {/* 3 Card Section */}
+                        <div className="flex justify-evenly mt-16 space-x-8">
+                            <div className="bg-gray-200 block max-w-sm p-6 border border-default rounded-4xl shadow-md">
+                                <a href="#">
+                                    <img className="rounded-base" src="/docs/images/blog/image-1.jpg" alt="" />
+                                </a>
+                                <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Planning</h5>
+                                <ul className="mb-6">
+                                    <li>Align tasks with project goals</li>
+                                    <li>Break down tasks into manageable steps</li>
+                                    <li>Set clear priorities and deadlines</li>
+                                </ul>
+                            </div>
+                            <div className="bg-gray-200 block max-w-sm p-6 border border-default rounded-4xl shadow-md">
+                                <a href="#">
+                                    <img className="rounded-base" src="/docs/images/blog/image-1.jpg" alt="" />
+                                </a>
+                                <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Tracking</h5>
+                                <ul className="mb-6">
+                                    <li>Track your works, your way</li>
+                                    <li>Manage risks and dependencies</li>
+                                    <li>Analyze progress and performance</li>
+                                </ul>
+                            </div>
+                            <div className="bg-gray-200 block max-w-sm p-6 border border-default rounded-4xl shadow-md">
+                                <a href="#">
+                                    <img className="rounded-base" src="/docs/images/blog/image-1.jpg" alt="" />
+                                </a>
+                                <h5 className="mt-6 mb-2 text-2xl font-semibold tracking-tight text-heading">Collaborate</h5>
+                                <ul className="mb-6">
+                                    <li>Automate workflows and notifications</li>
+                                    <li>Facilitate real-time communication</li>
+                                    <li>Share files and resources securely</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Option 2</h3>
-                        </div>
-                        <div>
-                            <h3>Option 3</h3>
-                        </div>
-                    </div>
                     </div>
                     {/* Get Start Button */}
                     <div className="text-center mt-12">
                         <span className="relative cursor-pointer">
                             <button>
                                 <a href="">
-                                    Get Start!!!    
+                                    Get Start!!!
                                 </a>
                             </button>
                         </span>
